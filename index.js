98056fbded68261d2f90b9c8b85c99c52f239b63
dadiprasanth <dadiprasanth24@gmail.com>
2022-11-17 21:18:07 +0530

    linked routed to the index page

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-
+const route=require("./routes/route")
 const express=require("express")
+const bodyparse=require("body-parser")
 const app=express()
+app.use(bodyparse.json())
+app.use("/",route)
 const port=8080
 app.listen(port,()=>console.log(`app is running at${port}`))
