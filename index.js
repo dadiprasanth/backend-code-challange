fcd74ed1d960a1433292023cd5e5d7dd9eb3e0ff
dadiprasanth <dadiprasanth24@gmail.com>
2022-11-17 21:49:31 +0530

    added data base to the index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const route=require("./routes/route")
+
 const express=require("express")
 const bodyparse=require("body-parser")
+const route=require("./routes/route")
+const { default: mongoose } = require("mongoose")
 const app=express()
 app.use(bodyparse.json())
 app.use("/",route)
+mongoose.connect("mongodb://localhost/challangebackend")
 const port=8080
-app.listen(port,()=>console.log(`app is running at${port}`))
+app.listen(port,()=>console.log(`app is running at${port}`))  
--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 const Schema=mongoose.Schema
 const BlogPost=new Schema({
     email:{type:String,required:true,unique:true},
-    password:{trpe:String,required:true}
+    password:{type:String,required:true}
 })
 const userdata=mongoose.model("userdata",BlogPost)
 module.exports=userdata;
